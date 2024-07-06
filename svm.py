cba575e8d52eed965797f584b68ec5d61dd5a8a0
CC-KEH <arbashhussain08@gmail.com>
2024-07-06 15:17:59 +0530

    SVM

--- svm.py
+++ svm.py
@@ -178,22 +178,17 @@ if __name__ == "__main__":
 
     visualize_svm(X_train, y_train, classifier, 'classification')
     
-    # #* Regression
+    #* Regression
     X, y = datasets.make_regression(n_samples=100, n_features=2, random_state=42)
 
-    # Split the dataset into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Initialize the SVM regressor
     regressor = SVM(mode='regression', epsilon=2)
 
-    # Fit the regressor to the training data
     regressor.fit(X_train, y_train)
 
-    # Make predictions on the testing set
     predictions = regressor.predict(X_test)
 
-    # Calculate and print the Mean Squared Error
     mse = mean_squared_error(y_test, predictions)
     print('MSE:', mse)
     
